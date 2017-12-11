4c0929f0b3da745ee6b1591037348796c0ffe0ef
shuajia <819418850@qq.com>
2017-12-11 14:10:18 +0800

    jia：

--- app/src/main/java/com/jia/rxtoolsandqmui/MainActivity.java
+++ app/src/main/java/com/jia/rxtoolsandqmui/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getMaxMemoryInfo();
+
     }
 
     private void getMaxMemoryInfo(){
